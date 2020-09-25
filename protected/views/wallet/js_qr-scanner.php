11d6cc3cb9717e3036c258837c8f2b2c461ddf57
Sergio Casizzone <jambtc@gmail.com>
2020-09-25 15:40:31 +0200

    estrae un eventuale importo dal qrcode

--- protected/views/wallet/js_qr-scanner.php
+++ protected/views/wallet/js_qr-scanner.php
@@ -1,16 +1,35 @@
 <script type="module">
-    import QrScanner from "./src/qr-scanner/qr-scanner.min.js";
-    QrScanner.WORKER_PATH = './src/qr-scanner/qr-scanner-worker.min.js';
+  import QrScanner from "./src/qr-scanner/qr-scanner.min.js";
+  QrScanner.WORKER_PATH = './src/qr-scanner/qr-scanner-worker.min.js';
 
-    const video = document.getElementById('qr-video');
-    const camQrResult = document.getElementById('WalletTokenForm_to');
+  const video = document.getElementById('qr-video');
+  const camQrResult = document.getElementById('WalletTokenForm_to');
 
-    function setResult(label, result) {
-		console.log('[QRCode result]',result);
-		$('#WalletTokenForm_to').val(result);
-		$('#scrollmodalCamera').modal('hide');
-		scanner.stop();
+  function setResult(label, result) {
+    console.log('[QRCode result]',result);
+
+    var res = extractImport(result);
+
+    $('#WalletTokenForm_to').val(res.address);
+    $('#WalletTokenForm_amount').val(res.amount);
+    $('#scrollmodalCamera').modal('hide');
+    scanner.stop();
+  }
+
+  // estrae un eventuale importo dal qrcode
+  function extractImport(result){
+    var str = result;
+    var spl = str.split("?",2);
+
+    if (spl[1] != 'undefinded'){
+      var amount = spl[1].split("=",2);
+      spl[1] = amount[1];
+    }else{
+      spl[1] = '';
     }
+    var ret = {'address':spl[0],'amount':spl[1]};
+    return ret;
+  }
 	const scanner = new QrScanner(video, result => setResult(camQrResult, result));
 
 	// al click del pulsante photo attivo la fotocamera
--- protected/yiic.php
+++ protected/yiic.php
@@ -1,7 +1,8 @@
 <?php
+$yiiVersion = 'yii-1.1.22';
 
 // change the following paths if necessary
-$yiic=dirname(__FILE__).'/../../yii/framework/yiic.php';
+$yiic=dirname(__FILE__).'/../../'.$yiiVersion.'/framework/yiic.php';
 $config=dirname(__FILE__).'/config/console.php';
 
 require_once($yiic);
